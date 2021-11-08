614cff4160862fcbc971855c68f6291f6c3eebf6
wanlei <wanlei@git.com>
2021-11-08 09:58:04 +0800

    idea project first commit

---
+++ src/main/java/com/wanlei/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.wanlei.git;
+
+/**
+ * @author TNT
+ * @create 2021-11-08
+ * @describe
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
