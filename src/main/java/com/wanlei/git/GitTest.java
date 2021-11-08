7833f0def2f68475e9658e30861b1d5f5d1d5c5c
wanlei <wanlei@git.com>
2021-11-08 10:15:28 +0800

    hot-fix first commit

--- src/main/java/com/wanlei/git/GitTest.java
+++ src/main/java/com/wanlei/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello GitHub");
         System.out.println("第三次提交");
+        System.out.println("Hello GitHub444444");
     }
 }
